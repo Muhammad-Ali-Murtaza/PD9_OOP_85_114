cdb2267ad52669f99f96c138b244cc02672b0d7e
Zeshan Ayub <unofficialmz@gmail.com>
2021-04-25 17:01:15 +0500

    Function

--- Lab Manual 9/src/Task4.java
+++ Lab Manual 9/src/Task4.java
@@ -97,13 +97,14 @@ class functions4{
     //This function validate that whole string consist of digits
     public boolean numberValidator(String s)
     {
+        boolean flag=true;
         for (int i = 0; i < s.length(); i++) {
             s = s.toLowerCase();
             if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
-                return false;
+                flag= false;
             }
             
         }
-        return true;
+        return flag;
     }
 }
